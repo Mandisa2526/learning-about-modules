bb965fb053ae034d4bd242e72c91f52a9e82d31f
root <root@LAPTOP-DK555H0M>
2023-07-19 14:58:43 +0200

    figlet function

--- greet_xola.js
+++ greet_xola.js
@@ -1,5 +1,19 @@
 import chalk from 'chalk';
+import figlet from 'figlet';
 import greet from './greet.js'
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+
+const greetXolaFiglet = greet('Xola');
+figlet(greetXolaFiglet,function(err,asciiArt){
+    if(err){
+        console("error generating asciiArt:",err);
+        return; 
+    }
+
+
+const styledMessage = chalk.bgGreen.black(greet(asciiArt));
+console.log(styledMessage);
+
+});
+
+
